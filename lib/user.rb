ca7076af360993bcfb1f525f6dcf4161a092c916
Pedro Loureiro <pedro.nunes.loureiro@gmail.com>
2019-05-19 22:54:37 +0100

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class User
+    attr_accessor :first_name, :last_name
+
+    def initialize(first_name = nil, last_name = nil)
+        @firs_tname = first_name
+        @last_name = last_name
+    end
+
+
+end
